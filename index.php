0818a83533887d4a618493fe7e88e1728737e6b6
Zloxen <51001794+zloxen@users.noreply.github.com>
2020-07-20 19:00:29 +0200

    main-upload

---
+++ index.php
@@ -0,0 +1,146 @@
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Jajuwa 2.0</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <link rel="stylesheet" href="/css/master.css">
+  </head>
+
+
+  <body>
+
+
+    <div class="jumbotron text-center" style="margin-bottom:0">
+      <h1>Jajuwa 2.0</h1>
+      <p>Najciemniejsze miejsce w internecie</p>
+    </div>
+
+
+
+
+
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark ">
+<div class="container-fluid">
+
+  <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarResponsive">
+    <span class="navbar-toggler-icon"></span>
+    <!-- zrobcie mi kawe pls -->
+  </button>
+  <div class="collapse navbar-collapse" id="navbarResponsive">
+    <ul class="navbar-nav ml-auto">
+
+
+      <li class="nav-item active">
+        <p>
+          <a class="nav-link"  data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">Dodaj Cytat</a>
+        </p>
+        <div class="collapse" id="collapseExample">
+          <div class="card card-body ">
+            <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Cytat</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="np. Słonie latają">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Inicjały</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="np. MG">
+  </div>
+
+  <div class="g-recaptcha" data-sitekey="6LcE2dsUAAAAAC8Ga3UFDPKIpC3tfjbB835iknyS"></div></br>
+
+  <button type="submit" class="btn btn-primary">Dodaj</button>
+</form>
+          </div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Informacje</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Dodaj Cytat</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://serwer1980543.home.pl/sql/" target="_blank">PHP MyAdmin</a>
+      </li>
+    </ul>
+  </div>
+</div>
+</nav>
+
+
+</div>
+
+
+
+
+<div class="container" style="margin-top:30px">
+  <div class="row">
+    <div class="col-sm-4">
+      <h2>Nowa Jajuwa 2.0!</h2>
+      <p>anka sj wciaga mefedron dupa test test 123 kurwadaaaaa</p>
+      <h3>Linki</h3>
+      <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Projekt "Facebook"</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+      </ul>
+      <hr class="d-sm-none">
+    </div>
+
+<div class="col-sm-8">
+
+
+  <table class="table">
+    <thead>
+      <tr>
+
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+
+        <td >Słonie latają</td>
+        <td>MG</td>
+      </tr>
+      <tr>
+
+        <td >Test nie ma jący na celu nic sprawdzić</td>
+        <td>JZ</td>
+      </tr>
+      <tr>
+
+        <td >Larry</td>
+        <td>the Bird</td>
+      </tr>
+    </tbody>
+  </table>
+
+
+  </div>
+
+  </div>
+</div>
+
+
+
+  </body>
+</html>
