145e6030620d1c1193636b4a7ce90490772b6000
Jakub Złotek <kubazlotek@gmail.com>
2020-12-13 17:11:35 +0100

    fixed ; in insert.php

--- Webpage v3 - new/server/insert.php
+++ Webpage v3 - new/server/insert.php
@@ -27,7 +27,7 @@ $osoba = $_POST["osoba"];
   $msg = new DiscordWebhook($webhook["url"]);
   $embed = new Embed();
   $desription = "IP: " . $_SERVER['REMOTE_ADDR'];
-  $embed->title("NOWY CYTAT NA JAJUWIE!")->description($desription)->setField($_POST["cytat"],$_POST["osoba"])
+  $embed->title("NOWY CYTAT NA JAJUWIE!")->description($desription)->setField($_POST["cytat"],$_POST["osoba"]);
   $msg->setUsername("ToWcaleNieBabelan w skrócie TWNB")->embed($embed)->send();
 
 
